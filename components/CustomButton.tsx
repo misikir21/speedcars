a0d45ebf379b681541de95981670e12820c7f41c
misikir21 <misikirkaka@gmail.com>
2024-02-23 17:41:04 +0300

    add c custom button into hero section

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,7 +1,22 @@
+"use client";
 import React from "react";
-
-function CustomButton() {
-  return <div>CustomButton</div>;
+import { CustomButtonProps } from "@/types";
+import Image from "next/image";
+function CustomButton({
+  title,
+  containerStyles,
+  handleClick,
+}: CustomButtonProps) {
+  return (
+    <button
+      disabled={false}
+      type={"button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={() => {}}
+    >
+      <span className={`flex-1`}>{title}</span>
+    </button>
+  );
 }
 
 export default CustomButton;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,8 @@
+"use client";
 import React from "react";
 import { CustomButton } from ".";
 function Hero() {
+  const handleScroll = () => {};
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
@@ -11,7 +13,11 @@ function Hero() {
           Streamline your car rental experieance with our effortless booking
           process
         </p>
-        <CustomButton />
+        <CustomButton
+          title="explore cars"
+          handleClick={handleScroll}
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+        />
       </div>
     </div>
   );
