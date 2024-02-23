cc9a2cfdcea857ee4c2e212465079eb158172007
misikir21 <misikirkaka@gmail.com>
2024-02-23 17:10:17 +0300

    add a header

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find,book or Renta car --quickly and easily!
+        </h1>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
