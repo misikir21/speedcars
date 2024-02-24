a86d76f3a0c345e3b3e852dec5a8a700c6a147bf
misikir21 <misikirkaka@gmail.com>
2024-02-24 10:14:46 +0300

    add a combo button

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,11 +1,28 @@
 import React from "react";
+import Image from "next/image";
 import { SearchManufacturerProps } from "@/types";
 import { Combobox, Transition } from "@headlessui/react";
 const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturerProps) => {
-  return <div>SearchManufacturer</div>;
+  return (
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top[14px]">
+            <Image
+              src="/car-logo.svg"
+              alt="car log"
+              width={20}
+              height={20}
+              className="ml-4"
+            />
+          </Combobox.Button>
+        </div>
+      </Combobox>
+    </div>
+  );
 };
 
 export default SearchManufacturer;
