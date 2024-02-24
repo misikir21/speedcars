9a43b32450fd3b61b61c6c08feb362ae5508175e
misikir21 <misikirkaka@gmail.com>
2024-02-24 09:04:04 +0300

    add a custom com

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,15 @@ export default function Home() {
       <Hero />
       <div className="mt-12 padding-x padding-y max-width" id="discover">
         <div className="home__text-container">
-          <h1 className="text-4xl">Car Catalouge</h1>
+          <h1 className="text-4xl font-extrabold">Car Catalouge</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+        <div className="home__filters">
+          <SearchBar />
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
         </div>
       </div>
     </main>
