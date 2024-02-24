1a90666fe151d15617a9a155461fe67855b6cbee
misikir21 <misikirkaka@gmail.com>
2024-02-24 12:26:48 +0300

    add a setter

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,11 +1,14 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+import { useState } from "react";
 import { SearchManufacturerProps } from "@/types";
 import { Combobox, Transition } from "@headlessui/react";
 const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturerProps) => {
+  const [query, setQuery] = useState("");
   return (
     <div className="search-manufacturer">
       <Combobox>
@@ -19,6 +22,12 @@ const SearchManufacturer = ({
               className="ml-4"
             />
           </Combobox.Button>
+          <Combobox.Input
+            className="search-manufacturer__input"
+            placeholder="volkswagen"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
         </div>
       </Combobox>
     </div>
