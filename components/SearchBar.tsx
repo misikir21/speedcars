70a88e2bca46c1765b141af17b0c940e4d0f5663
misikir21 <misikirkaka@gmail.com>
2024-02-24 09:19:00 +0300

    add a search manufacturer component

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,7 +1,15 @@
+"use client";
 import React from "react";
+import { SearchManufacturer } from ".";
 
 function SearchBar() {
-  return <div>SearchBar</div>;
+  return (
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer />
+      </div>
+    </form>
+  );
 }
 
 export default SearchBar;
--- components/index.tsx
+++ components/index.tsx
@@ -2,6 +2,15 @@ import Hero from "./Hero";
 import SearchBar from "./SearchBar";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
+import SearchManufacturer from "./SearchManufacturer";
 import CustomButton from "./CustomButton";
 import CustomFilter from "./CustomFilter";
-export { Hero, SearchBar, CustomFilter, NavBar, Footer, CustomButton };
+export {
+  Hero,
+  SearchBar,
+  SearchManufacturer,
+  CustomFilter,
+  NavBar,
+  Footer,
+  CustomButton,
+};
